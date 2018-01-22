78f1e1d9711668c35b70f90a127e11b34476afe1
Mikhail Kashkin <m@askpapir.com>
2018-01-22 17:21:26 +0200

    changed session execution order

--- sanic_session/__init__.py
+++ sanic_session/__init__.py
@@ -28,15 +28,18 @@ class Session:
 
         app.extensions['session'] = self
 
-        @app.middleware('request')
+        # @app.middleware('request')
         async def add_session_to_request(request):
             """Before each request initialize a session
             using the client's request."""
             await self.interface.open(request)
 
-        @app.middleware('response')
+        # @app.middleware('response')
         async def save_session(request, response):
             """After each request save the session, pass
             the response to set client cookies.
             """
             await self.interface.save(request, response)
+
+        app.request_middleware.appendleft(add_session_to_request)
+        app.response_middleware.append(save_session)
