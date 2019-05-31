e976b01a9768b72a8ad93d54dc6e86ef9f15a02e
Mikhail Kashkin <mkashkin@gmail.com>
2019-05-31 09:27:57 +0300

    new release

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
+venv/bin/activate:
+	python3 -m venv venv
+
+setup: venv/bin/activate dev_requirements.txt
+	. venv/bin/activate; pip install -U pip
+	. venv/bin/activate; pip install -Ur dev_requirements.txt
+
+
 release:
 	rm -r dist
 	. venv/bin/activate; python setup.py sdist bdist_wheel && twine upload dist/*
--- setup.py
+++ setup.py
@@ -4,30 +4,29 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 # Set requirements here
-requirements = ('sanic', 'ujson')
+requirements = ("sanic", "ujson")
 
 extras_require = {
-    'aioredis': ['aioredis>=1.0.0'],
-    'redis': ['asyncio_redis'],
-    'mongo': ['sanic_motor', 'pymongo'],
-    'aiomcache': ['aiomcache>=0.5.2'],
+    "aioredis": ["aioredis>=1.0.0"],
+    "redis": ["asyncio_redis"],
+    "mongo": ["sanic_motor", "pymongo"],
+    "aiomcache": ["aiomcache>=0.5.2"],
 }
 
 
 setup(
-    name='sanic_session',
-    version='0.5.0',
+    name="sanic_session",
+    version="0.5.4",
     description=(
-        'Provides server-backed sessions for Sanic '
-        'using Redis, Memcache and more.'
+        "Provides server-backed sessions for Sanic " "using Redis, Memcache and more."
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='http://github.com/subyraman/sanic_session',
-    author='Suby Raman, Mikhail Kashkin',
-    author_email='m@xen.ru',
-    license='MIT',
-    packages=['sanic_session'],
+    url="http://github.com/subyraman/sanic_session",
+    author="Suby Raman, Mikhail Kashkin",
+    author_email="m@xen.ru",
+    license="MIT",
+    packages=["sanic_session"],
     # Kludge: Specifying requirements for setup and install works around
     # problem with easyinstall finding sanic_motor instead of sanic.
     # See similar problem:
@@ -37,17 +36,17 @@ setup(
     install_requires=requirements,
     extras_require=extras_require,
     zip_safe=False,
-    keywords=['sessions', 'sanic', 'redis', 'memcache'],
+    keywords=["sessions", "sanic", "redis", "memcache"],
     classifiers=[
-        'Framework :: AsyncIO',
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: WWW/HTTP :: Session',
-    ]
+        "Framework :: AsyncIO",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet :: WWW/HTTP :: Session",
+    ],
 )
