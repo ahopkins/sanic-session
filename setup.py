776a7fd8ebca657ea194213af802df9a38532254
Mikhail Kashkin <mkashkin@gmail.com>
2020-01-07 10:12:00 +0200

    Bump release version for latest Sanic LTS release

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ extras_require = {
 
 setup(
     name="sanic_session",
-    version="0.5.6",
+    version="0.6.0",
     description=(
         "Provides server-backed sessions for Sanic " "using Redis, Memcache and more."
     ),
